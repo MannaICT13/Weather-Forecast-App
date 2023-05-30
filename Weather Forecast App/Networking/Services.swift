fc1420808b0dd869adc02a943b1bfedafe24e7d3
Md Khaled Hasan Manna <manna@sharetrip.net>
2023-05-30 23:49:46 +0600

    implement api client for fetching weather data

---
+++ Weather Forecast App/Networking/Services.swift
@@ -0,0 +1,43 @@
+//
+//  Services.swift
+//  Weather Forecast App
+//
+//  Created by Md Khaled Hasan Manna on 30/5/23.
+//
+
+import Foundation
+import Alamofire
+
+private struct WeatherRoute: APIRouteable {
+    
+    var baseURL: String = Environment.shared.baseURL.absoluteString
+    var path: String = "data/2.5/forecast"
+    var method: HTTPMethod = .get
+    var parameters: Parameter?
+    
+    struct Parameter: Codable {
+        let latitude: String
+        let longitude: String
+        let appid: String
+        let units: String
+        
+        enum CodingKeys: String, CodingKey {
+            case latitude = "lat"
+            case longitude = "lon"
+            case appid
+            case units
+        }
+    }
+    
+    init(parameters: Parameter) {
+        self.parameters = parameters
+    }
+}
+
+enum WeatherAPIClient: GenericAPIClient {
+    static func fetchWeatherInfo(latitude: String, longitude: String, appid: String, units: String, completion: @escaping(Result<NetworkResponse<[WeatherResponseList]>, AFError>) -> Void) {
+        let parameter = WeatherRoute.Parameter(latitude: latitude, longitude: longitude, appid: appid, units: units)
+        let route = WeatherRoute(parameters: parameter)
+        startRequest(with: route, completion: completion)
+    }
+}
