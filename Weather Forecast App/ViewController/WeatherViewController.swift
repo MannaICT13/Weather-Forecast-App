161edb367105a680f85ec215eab35d518a92a697
Md Khaled Hasan Manna <khmannaict13@gmail.com>
2023-05-31 23:15:21 +0600

    add a table view and implement  delegate and data source

--- Weather Forecast App/ViewController/WeatherViewController.swift
+++ Weather Forecast App/ViewController/WeatherViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class WeatherViewController: UIViewController {
     
+    @IBOutlet private weak var tableView: UITableView! {
+        didSet {
+            tableView.dataSource = self
+            tableView.delegate = self
+            tableView.registerNibHeaderFooter(CustomHeaderView.self)
+        }
+    }
+    
     let viewModel = WeatherViewModel()
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,3 +24,27 @@ class WeatherViewController: UIViewController {
     }
 }
 
+// MARK: - TableView DataSource & Delegate
+extension WeatherViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+// MARK: - TableView  Delegate
+extension WeatherViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = tableView.dequeueReusableHeaderFooterView() as CustomHeaderView
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 56.0
+    }
+}
