ceb65f9918c479d11a6adc7a5cd25d6a549430f1
Md Khaled Hasan Manna <khmannaict13@gmail.com>
2023-06-02 12:45:09 +0600

    create bicolour extension

---
+++ Weather Forecast App/Helpers/Extensions/UIColor+Aadditions.swift
@@ -0,0 +1,22 @@
+//
+//  UIColor+Aadditions.swift
+//  Weather Forecast App
+//
+//  Created by Md Khaled Hasan Manna on 2/6/23.
+//
+
+import UIKit
+
+extension UIColor {
+    static var sunnyBackgroundColor: UIColor {
+        return UIColor(red: 1.0, green: 0.8, blue: 0.0, alpha: 1.0)
+    }
+    
+    static var rainyBackgroundColor: UIColor {
+        return UIColor(red: 0.2, green: 0.4, blue: 0.8, alpha: 1.0)
+    }
+    
+    static var cloudyBackgroundColor: UIColor {
+        return UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1.0)
+    }
+}
