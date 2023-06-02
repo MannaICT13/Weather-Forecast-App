b2a992d4a702735d28a54852c4a9d2221816e4d2
Md Khaled Hasan Manna <khmannaict13@gmail.com>
2023-06-03 00:59:11 +0600

    add value to cell

--- Weather Forecast App/ViewController/SettingsViewController.swift
+++ Weather Forecast App/ViewController/SettingsViewController.swift
@@ -10,22 +10,17 @@ import UserNotifications
 
 class SettingsViewController: UIViewController {
     private let tableView = UITableView()
-    private let viewMModel = SettingsViewModel()
-    private let darkModeKey = Constants.shared.darkModeKey
-    private let celsiusKey = Constants.shared.celsiusKey
-    private let fehrenheitKey = Constants.shared.fahrenheitKey
-    private let latitudeKey = Constants.shared.latitude
-    private let longitudeKey = Constants.shared.longitude
+    private let viewModel = SettingsViewModel()
     private let userDefaults = UserDefaultsManager.shared
     
     private var isDarkModelEnabled: Bool {
         get {
-            if let value = userDefaults.value(forKey: darkModeKey ) as? Bool {
+            if let value = userDefaults.value(forKey: viewModel.darkModeKey ) as? Bool {
                 return value
             }
             return false
         }
-        set { userDefaults.set(value: newValue, forKey: darkModeKey) }
+        set { userDefaults.set(value: newValue, forKey: viewModel.darkModeKey) }
     }
     
     override func viewDidLoad() {
@@ -40,6 +35,13 @@ class SettingsViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        viewModel.callback.didSuccess = {[weak self] in
+            self?.userDefaults.set(value: self?.viewModel.city, forKey: self?.viewModel.cityKey ?? "city")
+            self?.tableView.reloadData()
+        }
+        viewModel.callback.didFailure = { error in
+            print(error)
+        }
         tableView.reloadData()
     }
     
@@ -108,27 +110,23 @@ class SettingsViewController: UIViewController {
 // MARK: - TableView Delegate & DataSource Methods
 extension SettingsViewController:  UITableViewDataSource, UITableViewDelegate {
     func numberOfSections(in tableView: UITableView) -> Int {
-        return viewMModel.sections.count
+        return viewModel.sections.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewMModel.sections[section].count
+        return viewModel.sections[section].count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cellType = viewMModel.sections[indexPath.section][indexPath.row]
+        let cellType = viewModel.sections[indexPath.section][indexPath.row]
         let cell = UITableViewCell(style: .value1, reuseIdentifier: nil)
         
         switch cellType {
         default:
             cell.textLabel?.text = cellType.title
             if cellType  == .defaultLocation {
-                if let lat = userDefaults.value(forKey: latitudeKey) as? Double,
-                   let lon = userDefaults.value(forKey: longitudeKey) as? Double {
-                    let latitude = String(format: "%.1f", lat)
-                    let longitude = String(format: "%.1f", lon)
-                    let location =  "lat:\(latitude) long: \(longitude)"
-                    cell.detailTextLabel?.text = location
+                if let city = userDefaults.value(forKey: viewModel.cityKey) as? String {
+                    cell.detailTextLabel?.text = city
                 }
             } else {
                 cell.detailTextLabel?.text = cellType.detail
@@ -139,14 +137,14 @@ extension SettingsViewController:  UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        let cellType = viewMModel.sections[indexPath.section][indexPath.row]
+        let cellType = viewModel.sections[indexPath.section][indexPath.row]
         switch cellType {
         case .celsius:
-            userDefaults.set(value: true, forKey: celsiusKey)
-            userDefaults.set(value: false, forKey: fehrenheitKey)
+            userDefaults.set(value: true, forKey: viewModel.celsiusKey)
+            userDefaults.set(value: false, forKey: viewModel.fehrenheitKey)
         case .fahrenheit:
-            userDefaults.set(value: false, forKey: celsiusKey)
-            userDefaults.set(value: true, forKey: fehrenheitKey)
+            userDefaults.set(value: false, forKey: viewModel.celsiusKey)
+            userDefaults.set(value: true, forKey: viewModel.fehrenheitKey)
         case .darkMode:
             isDarkModelEnabled.toggle()
             isDarkModelEnabled ? enableDarkMode() : disableDarkMode()
@@ -171,9 +169,10 @@ extension SettingsViewController:  UITableViewDataSource, UITableViewDelegate {
 extension SettingsViewController: LocationViewControllerDelegate {
     func getCoordinate(latitude: Double?, longitude: Double?) {
         if let latitude = latitude, let longitude = longitude {
-            userDefaults.set(value: latitude, forKey: self.latitudeKey)
-            userDefaults.set(value: longitude, forKey: self.longitudeKey)
+            userDefaults.set(value: latitude, forKey: self.viewModel.latitudeKey)
+            userDefaults.set(value: longitude, forKey: self.viewModel.longitudeKey)
         }
+        viewModel.fetchCity()
     }
 }
 
