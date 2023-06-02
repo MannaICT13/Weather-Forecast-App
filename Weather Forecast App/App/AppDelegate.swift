88ac72198c7199cbaf07f2650910a7351ae0cade
Md Khaled Hasan Manna <khmannaict13@gmail.com>
2023-06-02 15:25:02 +0600

    location permission when app start

--- Weather Forecast App/App/AppDelegate.swift
+++ Weather Forecast App/App/AppDelegate.swift
@@ -13,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        LocationManager.shared.checkLocationAuthorization()
         // Override point for customization after application launch.
         return true
     }
